6ab2a3b73f5225ddbcc19ecc9ce0c96639ce6d57
JonLucasWard <jonlucasward@gmail.com>
2023-11-30 17:49:55 -0500

    List on screen and sorting possible.

--- Project/src/main/java/com/spacex/Controllers/GreetingController.java
+++ Project/src/main/java/com/spacex/Controllers/GreetingController.java
@@ -1,5 +1,9 @@
 package com.spacex.Controllers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.alibaba.fastjson.JSONObject;
+
+import com.spacex.Objects.Rockets;
 
 @Controller
 public class GreetingController {
@@ -35,7 +41,27 @@ public class GreetingController {
 			 	.build().get().uri(url).retrieve().bodyToMono(JSONObject[].class).block();
 			System.out.println(response[0].getString("name"));
 
+			ArrayList<Rockets> rocketList = new ArrayList<Rockets>();
+
+			for(int i = 0;i<response.length;i++){
+				Rockets x = new Rockets(response[i]);
+				rocketList.add(x);
+				System.out.println("in the loop: " + rocketList.get(i).rocket.getString("name"));
+			}
+
+			// Collections.sort(rocketList);
+			// for(Rockets rocket:rocketList){
+			// 	System.out.println("first sort: " + rocket.rocket.getString("name"));
+			// }
+
+			// Collections.sort(rocketList, Collections.reverseOrder());
+			// for(Rockets rocket:rocketList){
+			// 	System.out.println("reverse sort: " + rocket.rocket.getString("name"));
+			// }
+
 		model.addAttribute("name", name);
+		//model.addAttribute("rockets", response);
+		model.addAttribute("rockets", rocketList);
 		return "rockets"; //return rockets.html
 	}
 
